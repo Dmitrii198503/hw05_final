f7afab051dc746195d14bfe09cbff82755f384ae
Дмитрий <golubtcovdmitrii@gmail.com>
2021-10-23 11:20:43 +0300

    final for review v.1.3

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -132,7 +132,7 @@ def follow_index(request):
 @login_required
 def profile_follow(request, username):
     user = request.user
-    author = User.objects.get(username=username)
+    author = get_object_or_404(User, username=username)
     if user != author:
         Follow.objects.get_or_create(user=user, author=author)
         return redirect('posts:profile', username)
@@ -142,9 +142,8 @@ def profile_follow(request, username):
 @login_required
 def profile_unfollow(request, username):
     user = request.user
-    author = User.objects.filter(username=username)[0]
     Follow.objects.get(
         user=user,
-        author=author
+        author__username=username
     ).delete()
     return redirect('posts:profile', username)
